932f4f3395ff277846e8fc73771112f4db50fe71
Aadi077 <78770737+Aadi077@users.noreply.github.com>
2021-07-01 09:51:18 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
 var database;
+
 var form, player, game;
-var gameState,playerCount;
+var car1, car2, car3, car4, cars;
 
-function setup() {
-    createCanvas(500, 500);
-   database = firebase.database();
-   game = new Game()
-   game.getState()
-   game.start();
+function setup(){
+  canvas = createCanvas(displayWidth-20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
-function draw() {
-}
-   
 
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
